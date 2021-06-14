25bd7c3231263b026013a1c686de2bbe20785e56
Alberto <ajsaenz1091@gmail.com>
2021-06-13 20:26:34 -0400

    Added dynamic style to container div in MenuItem component using imageUrl prop

--- src/Components/menu-item/MenuItem.js
+++ src/Components/menu-item/MenuItem.js
@@ -5,7 +5,9 @@ import './MenuItem.styles.scss'
 
 const MenuItem = ({id, imageUrl, linkUrl, title}) => {
     return(
-        <div className="menu-item">
+        <div style={{
+            backgroundImage: `url(${imageUrl})`
+        }} className="menu-item">
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
