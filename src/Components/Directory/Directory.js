e22cd2a0deac9ea56199710b94b71e58660ffa89
Alberto <ajsaenz1091@gmail.com>
2021-06-13 20:15:11 -0400

    added mock data to test directory component and passed props to menu item component

--- src/Components/Directory/Directory.js
+++ src/Components/Directory/Directory.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MenuItem from '../menu-item/MenuItem'
+import sectionsData from './directory.data'
 import './Directory.styles.scss'
 
 
 const Directory = () => {
+
+    const [sections, setSections] = useState(sectionsData)
+
+    const renderMenuItems = () => {
+        return sections.map((section, idx) => {
+            return <MenuItem key={idx} {...section} />
+        })
+    }
+
     return(
         <div className="directory-menu">
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
+            {renderMenuItems()}
         </div>
     )
 }
--- src/Components/menu-item/MenuItem.js
+++ src/Components/menu-item/MenuItem.js
@@ -3,11 +3,11 @@ import React from 'react'
 import './MenuItem.styles.scss'
 
 
-const MenuItem = () => {
+const MenuItem = ({id, imageUrl, linkUrl, title}) => {
     return(
         <div className="menu-item">
             <div className="content">
-                <h1 className="title">Product Category</h1>
+                <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
